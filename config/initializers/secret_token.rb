ec4ec4d0cdd40231fd6c5cb3c7fd2003a268731a
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-01-03 01:47:28 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCve20125664Test::Application.config.secret_token = 'e1e056d2fcfc690c70987973a17dfffa292d53334c7912f61ac53161581905878b484e8379530251d127949531cd3d91d39bc331563c20c7e346b59b57bbb69d'
